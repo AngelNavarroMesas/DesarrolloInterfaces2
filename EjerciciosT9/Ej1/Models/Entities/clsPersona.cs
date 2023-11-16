c993a1692ecbb4d8ee6c4e86199af084d1c157b3
AngelNavarroMesas <93648843+AngelNavarroMesas@users.noreply.github.com>
2023-11-16 14:39:33 +0100

    git

---
+++ EjerciciosT9/Ej1/Models/Entities/clsPersona.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1.Models.Entities
+{
+    internal class clsPersona : INotifyPropertyChanged
+    {
+        #region atributos
+        private string nombre;
+        private string apellido;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region constructores
+        public clsPersona()
+        {
+            this.nombre = "Angel";
+            this.apellido = "Navarro";
+        }
+
+        public clsPersona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        #endregion
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                nombre = value;
+                OnpropertyChanged();
+            }
+        }
+
+        public String Apellido
+        {
+            get { return apellido; }
+            set
+            {
+                apellido = value;
+                OnpropertyChanged();
+            }
+        }
+
+        public String NombreCompleto
+        {
+            get { return $"{nombre} {apellido}"; }
+        }
+        #endregion
+        public void OnpropertyChanged([CallerMemberName] string valor = "Angel")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(valor));
+        }
+    }
+}
